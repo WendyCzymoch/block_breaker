a7312a0b294e53a24d136920b17b15f8a185c2d3
Wendy Czymoch <wendy.czymoch@gmail.com>
2016-08-16 21:09:12 -0500

    basic ball and lose collider added

---
+++ Block Breaker/Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+
+public class LoseCollider : MonoBehaviour
+{
+
+    // Use this for initialization
+    void OnTriggerEnter2D(Collider2D trigger)
+    {
+        print ("Trigger");
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        print ("Collision");
+    }
+}
